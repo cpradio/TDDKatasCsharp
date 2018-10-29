affb286435af7888b7cb2548607181f7f0462ebb
Matthew Wilkin <wilkinm@grangeinsurance.com>
2018-10-29 14:51:25 -0400

    Better example of a mock, using verify

--- Tests/Mocking/Objects/SubmissionLogicTests.cs
+++ Tests/Mocking/Objects/SubmissionLogicTests.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Web.Mvc;
-using Katas.Mocking.DataAccess.Contexts;
-using Katas.Mocking.DataAccess.Controllers;
+﻿
 using Katas.Mocking.DataAccess.Entities;
-using Katas.Mocking.DataAccess.Entitites;
-using Katas.Mocking.DataAccess.Models;
 using Katas.Mocking.Objects.Logic;
 using Katas.Mocking.Objects.Wrapper;
 using Moq;
@@ -19,44 +11,40 @@ namespace Tests
 	public class SubmissionLogicTests
 	{
 		private SubmissionLogic _submissionLogic;
-		private bool _calledSave = false;
+		private Mock<ISubmissionWrapper> _mockSubmissionWrapper;
 
 		private void SetupWrapper(bool isValid)
 		{
-			var mockSubmissionWrapper = new Mock<ISubmissionWrapper>();
-			mockSubmissionWrapper.Setup(p => p.Submit()).Callback(() => _calledSave = true);
-			mockSubmissionWrapper.Setup(p => p.IsValid()).Returns(isValid);
+			_mockSubmissionWrapper = new Mock<ISubmissionWrapper>();
+			_mockSubmissionWrapper.Setup(p => p.Submit()).Verifiable();
+			_mockSubmissionWrapper.Setup(p => p.IsValid()).Returns(isValid);
 
 			_submissionLogic = new SubmissionLogic(new Submission())
 			{
-				SubmissionWrapper = mockSubmissionWrapper.Object
+				SubmissionWrapper = _mockSubmissionWrapper.Object
 			};
 		}
 
 		[Test]
 		public void validate_submission_is_saved_when_valid()
 		{
-			_calledSave = false;
-
 			var submission = new Submission();
 			SetupWrapper(true);
 			var result = _submissionLogic.ValidateAndSubmit(submission);
 
 			Assert.IsTrue(result);
-			Assert.IsTrue(_calledSave);
+			_mockSubmissionWrapper.Verify(a => a.Submit(), Times.Once);
 		}
 
 		[Test]
 		public void validate_submission_is_not_saved_when_invalid()
 		{
-			_calledSave = false;
-
 			var submission = new Submission();
 			SetupWrapper(false);
 			var result = _submissionLogic.ValidateAndSubmit(submission);
 
 			Assert.IsFalse(result);
-			Assert.IsFalse(_calledSave);
+			_mockSubmissionWrapper.Verify(a => a.Submit(), Times.Never);
 		}
 	}
 }
